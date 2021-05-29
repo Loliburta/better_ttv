eb6b98622b84a66b811ae6847885a89fe9fb3ee2
Loliburta <mateusz.maranowski@protonmail.com>
2021-05-29 15:27:52 +0200

    fixed routing for gh-pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,32 +1,18 @@
-import { useState } from "react";
 import { Navbar } from "./navbar/Navbar";
-import { TopGames } from "./topGames/TopGames";
-import { TopStreamers } from "./topStreamers/TopStreamers";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home } from "./home/Home";
 import { StreamersForGame } from "./streamersForGame/StreamersForGame";
 
 export const App = () => {
-  const [visibleGames, setVisibleGames] = useState(true);
-  const [, setVisiblePlayers] = useState(false);
-  const handleClickGames = () => {
-    setVisiblePlayers(false);
-    setVisibleGames(true);
-  };
-  const handleClickPlayers = () => {
-    setVisibleGames(false);
-    setVisiblePlayers(true);
-  };
-
   return (
     <Router>
       <div>
         <Navbar />
         <Switch>
-          <Route path="/" exact>
+          <Route path={["/", "/better_ttv"]} exact>
             <Home />
           </Route>
-          <Route path="/StreamersForGame/:GameName/:GameId">
+          <Route path="/better_ttv/StreamersForGame/:GameName/:GameId">
             <StreamersForGame />
           </Route>
         </Switch>
