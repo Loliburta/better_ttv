79db623149f74b81a0117cfedb18bb3df99b33ae
Loliburta <mateusz.maranowski@protonmail.com>
2021-05-30 18:01:26 +0200

    added home button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,22 @@
+import { Link } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import searchIcon from "@iconify-icons/akar-icons/search";
+import twitchIcon from "@iconify-icons/bi/twitch";
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <input
-        type="text"
-        className="navbar__search"
-        placeholder="Search"
-      ></input>
-      <div className="navbar__icon">
-        <Icon icon={searchIcon} className="navbar__icon__svg" />
+      <Link className="navbar__twitch" to={`${process.env.PUBLIC_URL}/`}>
+        <Icon icon={twitchIcon} />
+      </Link>
+      <div className="navbar__searchDiv">
+        <input
+          type="text"
+          className="navbar__searchDiv__search"
+          placeholder="Search"
+        ></input>
+        <div className="navbar__searchDiv__icon">
+          <Icon icon={searchIcon} className="navbar__searchDiv__icon__svg" />
+        </div>
       </div>
     </div>
   );
