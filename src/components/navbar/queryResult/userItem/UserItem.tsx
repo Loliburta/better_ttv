13a87bf53b912832740f6f0de3343255885ab725
Loliburta <mateusz.maranowski@protonmail.com>
2021-06-02 23:29:47 +0200

    added scrollbar and user item style

--- src/components/navbar/queryResult/userItem/UserItem.tsx
+++ src/components/navbar/queryResult/userItem/UserItem.tsx
@@ -1,3 +1,5 @@
+import { Icon } from "@iconify/react";
+import userIcon from "@iconify-icons/bx/bx-user";
 export interface UserItemProps {
   display_name: string;
   game_id: string;
@@ -13,29 +15,9 @@ export const UserItem: React.FC<UserItemProps> = ({
   is_live,
 }) => {
   return (
-    <div>
+    <div className="userItems__item">
+      <Icon className="userItems__item__icon" icon={userIcon} />
       <div>{display_name}</div>
     </div>
   );
 };
-
-// {
-//     "data": [
-//       {
-//         "broadcaster_language": "en",
-//         "broadcaster_login": "loserfruit",
-//         "display_name": "Loserfruit",
-//         "game_id": "498000",
-//         "game_name": "House Flipper",
-//         "id": "41245072",
-//         "is_live": false,
-//         "tags_ids": [],
-//         "thumbnail_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/fd17325a-7dc2-46c6-8617-e90ec259501c-profile_image-300x300.png",
-//         "title": "loserfruit",
-//         "started_at": ""
-//       },
-//   ... ],
-//     "pagination": {
-//       "cursor": "Mg=="
-//     }
-//   }
